3307a7f33f461222908405d523db8ae59db9ff86
DeepanS206 <deepan.saravanan83@gmail.com>
2014-10-25 16:30:04 -0400

    Changes

---
+++ BLASTOFF.java
@@ -0,0 +1,16 @@
+
+public class BLASTOFF {
+	public static void countdown(int n) {
+		if (n == 0) {
+			System.out.println("Blastoff");
+		} else {
+			System.out.println(n);
+			countdown(n-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		countdown(25);
+	}
+
+}
