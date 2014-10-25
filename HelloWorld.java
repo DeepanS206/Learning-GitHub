5f10210c6e41cf6e0810ebb54c598ddbf8d308d1
Deepan Saravanan <deepansaravanan@quad1-189.wireless-resnet.upenn.edu>
2014-10-25 16:04:02 -0400

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/* Name: Deepan Saravanan
+ * Pennkey: deepans
+ * Recitation: ???
+ * 
+ * Exeuction:java HelloWorld
+ * 
+ * Prints "Hello, World". By tradition, this is everyone's first program. 
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
